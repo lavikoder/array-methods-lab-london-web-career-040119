603495c57178a8c9e2bd44fc89b2f5cd2273e19b
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-24 20:10:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,14 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+  names_of = ["I", "bob", "wow"]
+  names_of.sort
 end
 
 def using_reverse(array)
-
+  array = ["arrays!", "bye", "wow"]
 end
 
 def using_first(array)
